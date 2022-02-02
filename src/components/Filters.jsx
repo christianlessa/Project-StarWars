c1db8f84c606290406b50865b8b976824121edee
Christian Lessa <christianclessa@gmail.com>
2022-02-02 00:17:55 -0300

    requisito 3 - Filtrando numeros na tabela

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,21 +2,72 @@ import React, { useContext } from 'react';
 import contextPlanets from '../context/contextPlanets';
 
 function Filters() {
-  const { setFilter } = useContext(contextPlanets);
+  const { setFilter, filterByNumericValues,
+    setFilterByNumericValues, handleClick } = useContext(contextPlanets);
 
   const handleChange = ({ target }) => {
     setFilter({
       filterByName: { name: target.value } });
   };
 
+  const handleSelect = ({ target: { name, value } }) => {
+    setFilterByNumericValues({
+      ...filterByNumericValues,
+      [name]: value,
+    });
+  };
+
+  const { column, comparison, value } = filterByNumericValues;
+
   return (
     <header>
-      <input
-        data-testid="name-filter"
-        type="text"
-        onChange={ handleChange }
-        placeholder="Pesquise o planeta"
-      />
+      <div>
+        <input
+          data-testid="name-filter"
+          placeholder="Pesquise o planeta"
+          onChange={ handleChange }
+          type="text"
+        />
+      </div>
+      <div>
+        <select
+          data-testid="column-filter"
+          onChange={ handleSelect }
+          value={ column }
+          name="column"
+        >
+          <option>population</option>
+          <option>orbital_period</option>
+          <option>diameter</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ handleSelect }
+          value={ comparison }
+          name="comparison"
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          data-testid="value-filter"
+          onChange={ handleSelect }
+          type="number"
+          min="0"
+          value={ value }
+          name="value"
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => handleClick(column, comparison, value) }
+        >
+          Filtrar
+        </button>
+      </div>
     </header>
   );
 }
--- src/context/ProviderPlanets.jsx
+++ src/context/ProviderPlanets.jsx
@@ -9,6 +9,11 @@ function ProviderPlanets({ children }) {
       name: '',
     },
   });
+  const [filterByNumericValues, setFilterByNumericValues] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: 0,
+  });
 
   useEffect(() => {
     fetch('https://swapi-trybe.herokuapp.com/api/planets/')
@@ -16,11 +21,29 @@ function ProviderPlanets({ children }) {
       .then((date) => setData((prevState) => prevState.concat(date.results)));
   }, []);
 
+  // Requisito 3 feito com ajuda do Gabriel Borges e Lucas Fernandes.
+
+  const handleClick = (column, comparison, value) => {
+    console.log(typeof (value));
+    switch (comparison) {
+    case 'maior que':
+      return setData(data.filter((planet) => (planet[column] > Number(value))));
+    case 'menor que':
+      return setData(data.filter((planet) => (planet[column] < Number(value))));
+    case 'igual a':
+      return setData(data.filter((planet) => (planet[column] === (value))));
+    default: break;
+    }
+  };
+
   const context = {
     data,
     setData,
     filter,
     setFilter,
+    filterByNumericValues,
+    setFilterByNumericValues,
+    handleClick,
   };
 
   return (
